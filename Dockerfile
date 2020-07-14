04ff29e89e634a998cd1c9269cc11dd10af0af05
Lee Jinseop <js428118@gmail.com>
2020-07-15 08:22:47 +0900

    Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:12-alpine
+RUN apk add --no-cache git make gcc g++ python3
+
+EXPOSE 3000
+
+ENV PORT=3000
+ENV NODE_ENV=production
+
+WORKDIR /app
+COPY . .
+RUN yarn install --production --frozen-lockfile
+CMD yarn start
---
+++ server.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { createServer } = require('http')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({
+  dev,
+  conf: {
+    publicRuntimeConfig: {
+      API_BASE_URL: process.env.API_BASE_URL,
+    },
+  },
+})
+const handler = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer(handler).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
